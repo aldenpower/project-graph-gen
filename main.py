5c7aae5c7bd1047ee6c3e6ca2ac55045fc232b4f
aldenpower <anderson_afsl@hotmail.com>
2022-04-06 12:27:51 -0300

    finish base graph

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import configparser
+import matplotlib.pyplot as plt
+import datetime as dt
 
 class GetConfig():
     def __init__(self, configfile):
         self.config = configparser.ConfigParser()
         self.config_ = self.config.read(configfile)
         self.settings = self.config['settings']
+        self.tittle = self.settings['tittle']
+        self.start = dt.date.fromisoformat(self.settings['start'])
+        self.end = dt.date.fromisoformat(self.settings['end'])
         self.planned = self.config['planned']
         self.accomplished = self.config['accomplished']
         self.replanned = self.config['replanned']
+        self.settings_values = self.get_values(self.settings)
+        self.planned_values = self.get_values(self.planned)
+        self.accomplished_values = self.get_values(self.accomplished)
+        self.replanned_values = self.get_values(self.replanned)
+        self.accomplished_eff = self.get_eff(self.accomplished_values[1], self.planned_values[1])
     def get_sections(self, show = True):
         if show:
             secs = self.config.sections()
@@ -20,7 +30,30 @@ class GetConfig():
             keys.append(key) 
         for value in range(0, len(keys)):
             var = section[keys[value]]
-            values.append(float(var))
+            if var.isdigit():
+                values.append(float(var))
+            else:
+                values.append(var)
 
         return keys, values
+    def get_eff(self, value1, value2):
+        eff = [a / b * 100 for a, b in zip(value1, value2)]
+        return eff 
 
+class GraphConfig():
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
+        self.fonttittle = {'family':'fantasy','color':'black','size': 22, 'fontweight' : "bold"}
+        self.font = {'family':'fantasy','color':'black','size':15}
+        self.font2 = {'family':'fantasy','color':'black','size':15}
+        self.bar_width = 0.2
+        self.tittle_pad = 60
+        self.xlabel_pad = 15.0
+    def autolabel(rectangle_group):
+        for rect in rectangle_group:
+            height = rect.get_height()
+    
+            self.ax.annotate(str(height), xy = (rect.get_x() + rect.get_width() / 4, height),
+            xytext = (0, 3), textcoords = 'offset points',
+            color = 'grey')
+    
--- run.py
+++ run.py
@@ -1,23 +1,82 @@
-from main import GetConfig
+from main import GetConfig, GraphConfig
 import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # Generate configuration object
 configuration        = GetConfig('config.cfg')
-
-# Load project settings
-settings             = configuration.settings    
-
-# Load project sections
-planned_section      = configuration.planned
-accomplished_section = configuration.accomplished
-replanned_section    = configuration.replanned
-
-# Get values
-planned_values       = configuration.get_values(planned_section)
-accomplished_values  = configuration.get_values(accomplished_section)
-replanned_values     = configuration.get_values(replanned_section)
+graph_config         = GraphConfig()
 
 if __name__ == '__main__':
-    print(planned_values)
-    print(accomplished_values)
-    print(replanned_values)
+    # Tittle configuration
+    graph_config.ax.set_title(
+    configuration.tittle,
+    fontdict = graph_config.fonttittle,
+    pad = graph_config.tittle_pad
+    )
+
+    # Settings text
+    graph_config.ax.text(
+    0.0,
+    max(configuration.accomplished_eff) + 10,
+    'Project start date : {}\nProject end date: {}\nToday : {}\nElapsed time: {} days'.
+    format(
+        configuration.start,
+        configuration.end,
+        dt.date.today(),
+        (dt.date.today() - configuration.start).days
+        ),
+    style='italic'
+    )
+
+    # Planned line
+    planned_line = graph_config.ax.plot(
+    configuration.planned_values[0],
+    configuration.planned_values[1],
+    marker = 'D',
+    color = 'green',
+    label = 'planned'
+    )
+    
+    # Accomplished line
+    accomplished_line = graph_config.ax.plot(
+    configuration.accomplished_values[0],
+    configuration.accomplished_values[1],
+    marker = 'h',
+    color = 'blue',
+    label = 'accomplished'
+    )
+    
+    # Replanned line
+    replanned_line = graph_config.ax.plot(
+    configuration.replanned_values[0],
+    configuration.replanned_values[1],
+    marker = 's',
+    color = 'red',
+    label = 'replanned'
+    )
+
+    # Positioning efficience bars
+    x_accomplished = [x for x in range(len(configuration.accomplished_eff))]
+
+    # Rectangle
+    rect1 = graph_config.ax.bar(
+    x_accomplished,
+    configuration.accomplished_eff,
+    graph_config.bar_width,
+    label = 'accomplished afficience',
+    color = 'darkslategray')
+
+    # Label names
+    graph_config.ax.set_xlabel(
+    'Weeks',
+    fontdict = graph_config.font,
+    labelpad = graph_config.xlabel_pad)
+
+    graph_config.ax.set_ylabel(
+    'Percentage (%)',
+    fontdict = graph_config.font2)
+
+    # Show the plot
+    plt.legend()
+    plt.show()
